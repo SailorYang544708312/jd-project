ec0268989fcb2930d83955dffdfbbcc9f4be0594
SailorYang544708312 <544708312@qq.com>
2020-05-11 22:51:59 +0800

    JD项目Day11完成开头

--- jd-search-service/src/main/java/com/jd/search/service/impl/ItemSearchServiceImpl.java
+++ jd-search-service/src/main/java/com/jd/search/service/impl/ItemSearchServiceImpl.java
@@ -5,13 +5,13 @@ import com.jd.pojo.TbItem;
 import com.jd.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.Query;
-import org.springframework.data.solr.core.query.SimpleQuery;
-import org.springframework.data.solr.core.query.result.ScoredPage;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service(timeout = 5000)
@@ -24,11 +24,36 @@ public class ItemSearchServiceImpl implements ItemSearchService {
    public Map<String, Object> search(Map<String, Object> searchMap) {
       Map<String, Object> map = new HashMap<String, Object>();
 
-      Query query = new SimpleQuery();
+      /*Query query = new SimpleQuery();
       //添加查询条件
       Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
       query.addCriteria(criteria);
-      ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
+      ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);*/
+
+      HighlightQuery query = new SimpleHighlightQuery();
+      HighlightOptions highlightOptions = new HighlightOptions().addField("item_title");//指定高亮的关键字出现在哪个域
+      highlightOptions.setSimplePrefix("<font style='color:red;'>");//高亮的前缀
+      highlightOptions.setSimplePostfix("</font>");//高亮的后缀
+      query.setHighlightOptions(highlightOptions);//设置高亮选项
+      //添加查询条件
+      Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+      query.addCriteria(criteria);
+      HighlightPage<TbItem> page = solrTemplate.queryForHighlightPage(query, TbItem.class);
+
+      /*List<HighlightEntry<TbItem>> entryList = page.getHighlighted();//这个集合中包含了高亮的信息
+      for (HighlightEntry<TbItem> entry : entryList) {
+         List<HighlightEntry.Highlight> highlights = entry.getHighlights();//获取高亮的列表
+         for (HighlightEntry.Highlight h : highlights) {
+            List<String> sns = h.getSnipplets();//每个域都有可能储存多个值
+            System.out.println(sns);
+         }
+      }*/
+      for (HighlightEntry<TbItem> h : page.getHighlighted()) { //循环高亮的入口
+         TbItem item = h.getEntity();  //获取到原实体类 (就是我们的item,其中title不带有高亮的)
+         if (h.getHighlights().size() > 0 && h.getHighlights().get(0).getSnipplets().size() > 0){
+            item.setTitle(h.getHighlights().get(0).getSnipplets().get(0));//设置高亮的结果集给item对象中的title
+         }
+      }
       //将分页的一页数据添加到map
       map.put("rows",page.getContent());
       return map;
--- jd-search-web/src/main/webapp/js/base.js
+++ jd-search-web/src/main/webapp/js/base.js
@@ -1,2 +1,9 @@
 //不分页的自定义模块
 var app = angular.module('jd',[]);
+
+//$sce: 封装了angularjs的信任策略
+app.filter('xinren',['$sce',function ($sce) {
+    return function (data) {
+        return $sce.trustAsHtml(data);
+    }
+}])
